f033854b2c0e10590f7bcf3542de222ce536327a
Xiao <constantine124@gmail.com>
2021-07-03 17:59:15 -0400

    Update

--- kv/storage/standalone_storage/standalone_storage.go
+++ kv/storage/standalone_storage/standalone_storage.go
@@ -4,28 +4,22 @@ import (
 	"github.com/Connor1996/badger"
 	"github.com/pingcap-incubator/tinykv/kv/config"
 	"github.com/pingcap-incubator/tinykv/kv/storage"
-	"github.com/pingcap-incubator/tinykv/log"
+	"github.com/pingcap-incubator/tinykv/kv/util/engine_util"
 	"github.com/pingcap-incubator/tinykv/proto/pkg/kvrpcpb"
 )
 
 // StandAloneStorage is an implementation of `Storage` for a single-node TinyKV instance. It does not
 // communicate with other nodes and all data is stored locally.
 type StandAloneStorage struct {
-	// Your Data Here (1).
-	db *badger.DB
+	Kv     *badger.DB
+	KvPath string
 }
 
 func NewStandAloneStorage(conf *config.Config) *StandAloneStorage {
-	// Your Code Here (1).
-	opt := badger.DefaultOptions
-	opt.Dir = conf.DBPath
-	opt.ValueDir = conf.DBPath
-	db, err := badger.Open(opt)
-	if err != nil {
-		log.Fatal(err)
-	}
+	db := engine_util.CreateDB(conf.DBPath, false)
 	return &StandAloneStorage{
-		db: db,
+		Kv:     db,
+		KvPath: conf.DBPath,
 	}
 }
 
@@ -36,15 +30,26 @@ func (s *StandAloneStorage) Start() error {
 
 func (s *StandAloneStorage) Stop() error {
 	// Your Code Here (1).
+	if err := s.Kv.Close(); err != nil {
+		return nil
+	}
 	return nil
 }
 
 func (s *StandAloneStorage) Reader(ctx *kvrpcpb.Context) (storage.StorageReader, error) {
 	// Your Code Here (1).
+
 	return nil, nil
 }
 
 func (s *StandAloneStorage) Write(ctx *kvrpcpb.Context, batch []storage.Modify) error {
-	// Your Code Here (1).
+	for _, m := range batch {
+		switch m.Data.(type) {
+		case storage.Delete:
+			engine_util.DeleteCF(s.Kv, m.Cf(), m.Key())
+		case storage.Put:
+			engine_util.PutCF(s.Kv, m.Cf(), m.Key(), m.Value())
+		}
+	}
 	return nil
 }
--- kv/storage/standalone_storage/standalone_storage_test.go
+++ kv/storage/standalone_storage/standalone_storage_test.go
@@ -7,12 +7,13 @@ import (
 	 *"github.com/Connor1996/badger"
 	 */
 	"github.com/pingcap-incubator/tinykv/kv/config"
+	"github.com/pingcap-incubator/tinykv/kv/storage"
 	"github.com/pingcap-incubator/tinykv/log"
 	"github.com/stretchr/testify/require"
 )
 
 func txnSet(t *testing.T, kv *StandAloneStorage, key []byte, val []byte, meta byte) {
-	txn := kv.db.NewTransaction(true)
+	txn := kv.Kv.NewTransaction(true)
 	require.NoError(t, txn.SetWithMeta(key, val, meta))
 	require.NoError(t, txn.Commit())
 }
@@ -20,10 +21,16 @@ func txnSet(t *testing.T, kv *StandAloneStorage, key []byte, val []byte, meta by
 func TestNewStandAloneStorage(t *testing.T) {
 	conf := config.NewTestConfig()
 	s := NewStandAloneStorage(conf)
-	log.Info(s.db)
+	log.Info(s.Kv, s.KvPath)
 	// DB instance
 	for i := 0; i < 100; i++ {
 		txnSet(t, s, []byte(fmt.Sprintf("key%d", i)), []byte(fmt.Sprintf("val%d", i)), 0x00)
 	}
+	add := storage.Put{[]byte("a"), []byte("a1"), "test"}
+	del := storage.Delete{[]byte("a"), "test"}
+	batch := []storage.Modify{{add}, {del}}
 
+	require.NoError(t, s.Write(nil, batch))
+
+	require.NoError(t, s.Stop())
 }
