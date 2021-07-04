26cfb2b63c290bd7c22314fdc8d8ce2f06641f7d
Xiao Cui <constantine124@gmail.com>
2021-07-04 17:57:28 -0400

    Add more unit test

--- kv/storage/standalone_storage/standalone_storage_test.go
+++ kv/storage/standalone_storage/standalone_storage_test.go
@@ -1,7 +1,6 @@
 package standalone_storage
 
 import (
-	"fmt"
 	"os"
 	"testing"
 	/*
@@ -10,16 +9,8 @@ import (
 	"github.com/pingcap-incubator/tinykv/kv/config"
 	"github.com/pingcap-incubator/tinykv/kv/storage"
 	"github.com/pingcap-incubator/tinykv/kv/util/engine_util"
-	"github.com/pingcap-incubator/tinykv/log"
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 )
-
-func txnSet(t *testing.T, kv *StandAloneStorage, key []byte, val []byte, meta byte) {
-	txn := kv.Kv.NewTransaction(true)
-	require.NoError(t, txn.SetWithMeta(key, val, meta))
-	require.NoError(t, txn.Commit())
-}
 
 func Set(s *StandAloneStorage, cf string, key []byte, value []byte) error {
 	return s.Write(nil, []storage.Modify{
@@ -28,6 +19,17 @@ func Set(s *StandAloneStorage, cf string, key []byte, value []byte) error {
 				Cf:    cf,
 				Key:   key,
 				Value: value,
+			},
+		},
+	})
+}
+
+func Delete(s *StandAloneStorage, cf string, key []byte) error {
+	return s.Write(nil, []storage.Modify{
+		{
+			Data: storage.Delete{
+				Cf:  cf,
+				Key: key,
 			},
 		},
 	})
@@ -56,23 +58,6 @@ func cleanUpTestData(conf *config.Config) error {
 	return nil
 }
 
-func TestNewStandAloneStorage(t *testing.T) {
-	conf := config.NewTestConfig()
-	s := NewStandAloneStorage(conf)
-	log.Info(s.Kv, s.KvPath)
-	// DB instance
-	for i := 0; i < 100; i++ {
-		txnSet(t, s, []byte(fmt.Sprintf("key%d", i)), []byte(fmt.Sprintf("val%d", i)), 0x00)
-	}
-	add := storage.Put{[]byte("a"), []byte("a1"), "test"}
-	del := storage.Delete{[]byte("a"), "test"}
-	batch := []storage.Modify{{add}, {del}}
-
-	require.NoError(t, s.Write(nil, batch))
-
-	require.NoError(t, s.Stop())
-}
-
 func TestRawGet(t *testing.T) {
 	conf := config.NewTestConfig()
 	s := NewStandAloneStorage(conf)
@@ -85,4 +70,57 @@ func TestRawGet(t *testing.T) {
 
 	val, _ := Get(s, cf, []byte{99})
 	assert.Equal(t, []byte{42}, val)
+}
+
+func TestRawGetNotFound(t *testing.T) {
+	conf := config.NewTestConfig()
+	s := NewStandAloneStorage(conf)
+	s.Start()
+	defer cleanUpTestData(conf)
+	defer s.Stop()
+	cf := engine_util.CfDefault
+	val, _ := Get(s, cf, []byte{99})
+	assert.Nil(t, val)
+}
+
+func TestRawDelete(t *testing.T) {
+	conf := config.NewTestConfig()
+	s := NewStandAloneStorage(conf)
+	s.Start()
+	defer cleanUpTestData(conf)
+	defer s.Stop()
+	cf := engine_util.CfDefault
+	assert.Nil(t, Set(s, cf, []byte{99}, []byte{42}))
+
+	err := Delete(s, cf, []byte{99})
+	assert.Nil(t, err)
+
+	val, _ := Get(s, cf, []byte{99})
+	assert.Equal(t, []byte(nil), val)
+}
+
+func TestRawScan(t *testing.T) {
+	conf := config.NewTestConfig()
+	s := NewStandAloneStorage(conf)
+	s.Start()
+	defer cleanUpTestData(conf)
+	defer s.Stop()
+	cf := engine_util.CfDefault
+
+	Set(s, cf, []byte{1}, []byte{233, 1})
+	Set(s, cf, []byte{2}, []byte{233, 2})
+	Set(s, cf, []byte{3}, []byte{233, 3})
+	Set(s, cf, []byte{4}, []byte{233, 4})
+	Set(s, cf, []byte{5}, []byte{233, 5})
+
+	itr, err := Iter(s, cf)
+	assert.Nil(t, err)
+	expectedKeys := [][]byte{{1}, {2}, {3}, {4}, {5}}
+	i := 0
+	for itr.Seek([]byte{1}); itr.Valid(); itr.Next() {
+		item := itr.Item()
+		k := item.Key()
+		assert.Equal(t, expectedKeys[i], k)
+		i++
+	}
 }
